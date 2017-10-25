62ae50c1afc7661666b3c8b42b4d416e35f25f54
Jay Stevens <disneylandjay@gmail.com>
2017-10-24 19:35:42 -0700

    Removed dependency to the old StateMachine plugin.

--- Source/DungeonMaker/DungeonMaker.Build.cs
+++ Source/DungeonMaker/DungeonMaker.Build.cs
@@ -10,7 +10,7 @@ public class DungeonMaker : ModuleRules
 		
 		PublicIncludePaths.AddRange(
 			new string[] {
-                "DungeonMaker/Public",
+				"DungeonMaker/Public",
 				// ... add public include paths required here ...
 			}
 			);
@@ -18,7 +18,7 @@ public class DungeonMaker : ModuleRules
 		
 		PrivateIncludePaths.AddRange(
 			new string[] {
-                "DungeonMaker/Private",
+				"DungeonMaker/Private",
 				// ... add other private include paths required here ...
 			}
 			);
@@ -39,8 +39,7 @@ public class DungeonMaker : ModuleRules
 				"CoreUObject",
 				"Engine",
 				"Slate",
-				"SlateCore",
-                "StateMachine",
+				"SlateCore"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
--- Source/DungeonMaker/Public/Mission/DungeonMissionNode.h
+++ Source/DungeonMaker/Public/Mission/DungeonMissionNode.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
-#include "StateMachine.h"
 #include "GraphNode.h"
 #include "DungeonMissionNode.generated.h"
 
