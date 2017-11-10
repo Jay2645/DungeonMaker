cbc294fc4f25abda36cd69e6fdcfc56bdf697305
Jay Stevens <disneylandjay@gmail.com>
2017-11-10 03:32:43 -0800

    Added new type of room -- a trial room, for puzzles characters must solve.

---
+++ Source/DungeonMaker/Public/Space/Rooms/TrialRoomBase.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Space/Rooms/DungeonRoom.h"
+#include "TrialRoomBase.generated.h"
+
+/**
+ * An example room, forming the base class for a "trial".
+ * These trials are puzzles forcing the player to test their skill.
+ */
+UCLASS(Abstract)
+class DUNGEONMAKER_API ATrialRoomBase : public ADungeonRoom
+{
+	GENERATED_BODY()	
+};
